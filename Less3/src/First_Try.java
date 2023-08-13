14e9e873773ab34e2d63aca84dcb938ae1707a12
EmelyaI <amalovskii@gmail.com>
2023-08-13 22:21:44 +0300

    ThirdCommMain

--- Less3/src/First_Try.java
+++ Less3/src/First_Try.java
@@ -1,5 +1,8 @@
-public class First_Try {
+//Создать новый проект, запушить его на удалённый репозиторий GitHub. Сделать
+//        несколько коммитов в мастер ветке. Создать отдельную ветку, переключиться на неё и
+//        сделать несколько коммитов. Смержить в мастер ветку.
+ class First_Try {
     public static void main(String[] args) {
-        System.out.println("FirstCommBranch");
+        System.out.println("ThirdCommBranch2");
     }
 }
