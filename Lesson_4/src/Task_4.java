5cd7930b73d12b5d5f0bbfc774c6956ce5d28995
EmelyaI <amalovskii@gmail.com>
2023-08-14 15:09:06 +0300

    Hw4_4

---
+++ Lesson_4/src/Task_4.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrSize;
+        System.out.println("Please input array size > 0:");
+        if (scanner.hasNextInt()) {
+            arrSize = scanner.nextInt();
+        } else {
+            System.out.println("Incorrect input.");
+            return;
+        }
+        if (arrSize <= 0) {
+            System.out.println("Incorrect input.");
+            return;
+
+        }
+///////////////////////////////////////////////////////////////////////////////////////
+        int[] arr = new int[arrSize];
+        for (int f = 0; f < arrSize; f++) {
+            arr[f] = Math.round((float) (Math.random() * 10));
+        }
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+                counter++;
+            }
+        }
+        if(counter == 0){
+            System.out.println("array has no 0");
+        } else {
+            System.out.println("0 in array - " + counter);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
