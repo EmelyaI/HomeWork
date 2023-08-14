9409438fc53aaa986b0cd01e59dcf2fefea602f9
EmelyaI <amalovskii@gmail.com>
2023-08-14 13:41:21 +0300

    Less4_1

--- Lesson_4/src/Task_1.java
+++ Lesson_4/src/Task_1.java
@@ -1,3 +1,5 @@
+/*Задача 1:
+        Пройти по массиву, вывести все элементы в прямом и в обратном порядке*/
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -19,8 +21,34 @@ public class Task_1 {
         {
             System.out.println("Incorrect input.");
             return;
-
         }
+        ///////////////////////////////////////////////////////////////////////////////////////
+        int buf;
+        int[] arr = new int[arrSize];
+        for(int f = 0; f < arrSize; f++){
+            arr[f] = Math.round((float) (Math.random() * 10));
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    buf = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = buf;
+                }
 
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 ; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    buf = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = buf;
+                }
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
