ab9cefa472e7bdb0a28d7b2ff157d55e77dac64d
EmelyaI <amalovskii@gmail.com>
2023-08-20 21:13:49 +0300

    Homework 5_2

--- Lesson_5/src/Task_2.java
+++ Lesson_5/src/Task_2.java
@@ -1,9 +1,25 @@
 public class Task_2 {
     public static void main(String[] args) {
         String[][] arr = new String[8][8];
+        String W = "W";
+        String B = "B";
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(" " + arr[i][j]);
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    arr[i][j] = W;
+                    System.out.print(" " + arr[i][j]);
+                } else
+                {
+                    if (j % 2 != 0 && i % 2 != 0)
+                    {
+                        arr[i][j] = W;
+                        System.out.print(" " + arr[i][j]);
+                    }else {
+                        arr[i][j] = B;
+                        System.out.print(" " + arr[i][j]);
+                    }
+                }
             }
             System.out.println();
         }
