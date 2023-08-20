e0260f38f4347aac684502d3dd9ad54ae25060aa
EmelyaI <amalovskii@gmail.com>
2023-08-20 22:07:26 +0300

    Homework 5_Star

--- Lesson_5/src/Task_3.java
+++ Lesson_5/src/Task_3.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_3 {
@@ -6,12 +7,25 @@ public class Task_3 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
+        int c = 0;
         int[][] arr = new int[n][m];
-            for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println();
+            if (i % 2 == 0) {
                 for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = +c;
+                    c++;
                     System.out.print(" " + arr[i][j]);
                 }
-                System.out.println();
+            } else {
+                for (int j = arr[i].length - 1; j >= 0; j--) {
+                    arr[i][j] = c;
+                    c++;
+                }
+                for (int g = 0; g < arr[i].length; g++) {
+                    System.out.print(" " + arr[i][g]);
+                }
             }
+        }
     }
 }
