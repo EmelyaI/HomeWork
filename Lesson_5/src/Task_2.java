b62f57c0cce39d0f40f43432aa92fc48218f61e8
EmelyaI <amalovskii@gmail.com>
2023-08-20 20:04:09 +0300

    Homework 5_2

---
+++ Lesson_5/src/Task_2.java
@@ -0,0 +1,10 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        String[][] arr = new String[8][8];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+            }
+            System.out.println();
+        }
+    }
+}
