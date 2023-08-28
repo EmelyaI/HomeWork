731005d8da1abc171c3f310db34124c3febaa003
EmelyaI <72811772+EmelyaI@users.noreply.github.com>
2023-08-28 19:01:57 +0300

    Revert "Task 7 1"

--- Lesson_7/src/Task_1_Clinic/Surgeon.java
+++
@@ -1,8 +0,0 @@
-package Task_1_Clinic;
-
-public class Surgeon extends Doctor {
-    @Override
-    public void TreatmentMethod() {
-        System.out.println("Treatment method - surgeon");
-    }
-}
