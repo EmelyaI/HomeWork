5432945362312edcb0d4223ca2b838ade3fa4a9c
Emelya <amalovskii@gmail.com>
2023-09-13 00:43:26 +0300

    Lesson 12 is done

--- Lesson_12/src/Main.java
+++ Lesson_12/src/Main.java
@@ -1,12 +1,31 @@
 /*В задании приложен файл Ромео и Джульетта на английском, вычитать его в приложении.
         Проанализировать и записать в другой файл самое длинное слово.*/
 
-import java.io.File;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File ("C:\\Future disc D\\Курсы\\HomeWorkk\\Resources\\RandJ.txt");
-        System.out.println(file.exists());
+        File file1 = new File("C:\\Future disc D\\Курсы\\HomeWorkk\\Resources\\RandJ.txt");
+        File file2 = new File("C:\\Future disc D\\Курсы\\HomeWorkk\\Resources\\RandJNew.txt");
+        try {
+            FileReader fileReader = new FileReader(file1);
+            FileWriter fileWriter = new FileWriter(file2);
+
+            char[] cont = new char[145];
+            fileReader.read(cont);
+
+            Longest longest = new Longest(cont);
+            longest.longestWord(cont);
+            String longestWord = longest.getLongestWord1();
+
+            fileWriter.write(longestWord + " - is the longest word in text");
+
+            fileReader.close();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
