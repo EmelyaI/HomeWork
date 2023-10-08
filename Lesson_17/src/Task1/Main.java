226bfb2899828a84af6374d96113bbee38c5ad5c
Emelya <amalovskii@gmail.com>
2023-10-08 23:17:04 +0300

    Lesson 17.1 is done

---
+++ Lesson_17/src/Task1/Main.java
@@ -0,0 +1,23 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*Задача 1:
+        Вывести в консоль из строки которую пользователь вводит с клавиатуры все
+        аббревиатуры. Аббревиатурой будем считать слово от 2 до 6 символов, состоящее
+        только из прописных букв, без чисел.*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Pattern pattern = Pattern.compile("\\b[a-z]{2,6}\\b");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
